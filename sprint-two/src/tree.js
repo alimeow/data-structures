c79bc51ee54b08e0734e234179b656fce543cd45
Ali yoho <studycatcat@yahoo.com>
2022-02-27 21:08:02 -0800

    draft commit

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,22 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
+  newTree.children = []; // fix me
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var found = false;
+
+
 };
 
 
